a51ed404a244c65190ecd184a0b78a478a3eacac
Loke <U2507983@rts-365.dk>
2025-02-03 12:54:09 +0100

    services content added

--- js/index.js
+++ js/index.js
@@ -10,18 +10,45 @@ const footerSection = document.querySelector(".footer");
 
 // ----- HERO ----- //
 let heroImg = document.createElement("img");
-heroImg.classList.add("hero__img")
+heroImg.classList.add("hero__img");
 
-let heroHeading = document.createElement("h1");
-heroHeading.classList.add("hero__heading")
-heroHeading.textContent = hero.headline
+let heroHeadline = document.createElement("h1");
+heroHeadline.classList.add("hero__headline");
+heroHeadline.textContent = hero.headline;
 
 let heroTxt = document.createElement("p");
-heroTxt.classList.add("hero__txt")
-heroTxt.textContent = hero.copy
+heroTxt.classList.add("hero__txt");
+heroTxt.textContent = hero.copy;
+
+heroSection.append(heroImg, heroHeadline, heroTxt);
 
 
 // ----- SERVICES ----- //
+
+services.forEach(card => {
+    let servicesCard = document.createElement("article");
+    servicesCard.classList.add("services__card");
+
+    let servicesCardImg = document.createElement("img");
+    servicesCardImg.classList.add("services__card__headline");
+    servicesCardImg.setAttribute("src", card.illustration)
+
+    let servicesCardHeadline = document.createElement("h2");
+    servicesCardHeadline.classList.add("services__card__headline");
+    servicesCardHeadline.textContent = card.headline;
+
+    let servicesCardTxt = document.createElement("p");
+    servicesCardTxt.classList.add("services__card__txt");
+    servicesCardTxt.textContent = card.text;
+
+    let servicesCardLink = document.createElement("a");
+    servicesCardLink.classList.add("services__card__link");
+    servicesCardLink.textContent = card.linktext;
+
+    servicesCard.append(servicesCardImg, servicesCardHeadline, servicesCardTxt, servicesCardLink);
+    servicesSection.append(servicesCard);
+});
+
 
 
 // ----- FACILITIES ----- //
@@ -34,7 +61,3 @@ heroTxt.textContent = hero.copy
 
 
 // ----- FOOTER ----- //
-
-
-// ----- APPEND ----- //
-heroSection.append(heroImg, heroHeading, heroTxt)
