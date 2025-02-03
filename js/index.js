084c29cff720d846c0c636c209316e68ef865dc9
Loke <U2507983@rts-365.dk>
2025-02-03 12:43:21 +0100

    hero content added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,40 @@
-// slå dig løs her... 
+// ----- SECTIONS ----- //
+const heroSection = document.querySelector(".hero");
+const servicesSection = document.querySelector(".services");
+const facilitiesSection = document.querySelector(".facilities");
+const sitesSection = document.querySelector(".sites");
+const advantagesSection = document.querySelector(".advantages");
+const footerSection = document.querySelector(".footer");
 
-//eksempel på at udskrive alle overskrifter i services i konsollen:
-services.forEach(service => console.log(service.headline))
+
+
+// ----- HERO ----- //
+let heroImg = document.createElement("img");
+heroImg.classList.add("hero__img")
+
+let heroHeading = document.createElement("h1");
+heroHeading.classList.add("hero__heading")
+heroHeading.textContent = hero.headline
+
+let heroTxt = document.createElement("p");
+heroTxt.classList.add("hero__txt")
+heroTxt.textContent = hero.copy
+
+
+// ----- SERVICES ----- //
+
+
+// ----- FACILITIES ----- //
+
+
+// ----- SITES ----- //
+
+
+// ----- ADVANTAGES ----- //
+
+
+// ----- FOOTER ----- //
+
+
+// ----- APPEND ----- //
+heroSection.append(heroImg, heroHeading, heroTxt)
